41f31a20182b77956287ddb98b100994487c78dd
ikuymark2490-coder <ikuymark2490@gmail.com>
2026-03-19 03:45:18 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,29 @@
-const CACHE_NAME = 'my-money-cache-v1';
+// เปลี่ยนเลข v1 เป็น v2, v3, v4 ไปเรื่อยๆ ทุกครั้งที่มีการอัปเดต index.html
+const CACHE_NAME = 'my-money-cache-v1'; 
 const urlsToCache = [
   './index.html'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // บังคับให้อัปเดตทันที
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// ระบบล้างแคช (ความจำ) ตัวเก่าทิ้ง เวลาเลขเวอร์ชันเปลี่ยน
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
