c6c73c33829d056705e2d3e174be401bc45f327d
ikuymark2490-coder <ikuymark2490@gmail.com>
2026-03-19 20:41:44 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ⚡️ อัปเดตเลขเวอร์ชันเป็น v5 เพื่อล้างท่อใหม่ทั้งหมด
-const CACHE_NAME = 'my-money-cache-v5'; 
+const CACHE_NAME = 'my-money-cache-v6'; 
 const urlsToCache = [
   './',
   './index.html',
